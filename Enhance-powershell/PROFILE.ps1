d69777463762c9c25cc8a3f3352c9b2b12f49983
Ash <spirizeon@proton.me>
2023-05-31 11:32:38 +0530

    adjusted function names

--- Enhance-powershell/PROFILE.ps1
+++ Enhance-powershell/PROFILE.ps1
@@ -1,10 +1,10 @@
 clear
 
-function yet { 
+function play-youtube { 
 	cd C:\Users\geniu\Downloads\mepoov\
 	yt
 }
-Set-Alias -Name yt -Value yet
+Set-Alias -Name yt -Value play-youtube
 
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
@@ -13,11 +13,11 @@ invoke-expression (&starship init powershell)
 Set-Alias -Name hx -Value "C:\Users\geniu\AppData\Local\Microsoft\WinGet\Packages\Helix.Helix_Microsoft.Winget.Source_8wekyb3d8bbwe\helix-23.03-x86_64-windows\hx.exe"
 Set-Alias -Name exercism -Value "C:/Users/geniu/exercism.exe"
 
-function dup {
+function duplicate-path {
 	$pwd.path | clip
 	echo "Path copied!"
 }
-Set-Alias -Name dup -Value dup
+Set-Alias -Name dup -Value duplicate-path
 
 
 function helixopen {
